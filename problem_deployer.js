0efeaf5514365b3ea9aa68ca6b6b38e0013e5415
Yeonjun Lee <yeonfish6040@gmail.com>
2025-02-15 19:59:23 +0900

    feat: dockerfile location custom

--- problem_deployer.js
+++ problem_deployer.js
@@ -157,7 +157,7 @@ async function deploy() {
       case "container":
         // docker build
         const debug1 = await new Promise((accept) => {
-          child_process.exec(`docker build . -t ${base32_file}`, { cwd: path.join(problem_dir, file) }, accept);
+          child_process.exec(`docker build . -t ${base32_file}`, { cwd: path.join(problem_dir, file, config("DOCKER_LOCATION")) || path.join(problem_dir, file) }, accept);
         });
         if (debug1) throw new Error(`${debug1}`);
         register_config["type"] = "container";
